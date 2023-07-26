7e75554dacd1647a73fd7c535b3798f2d718fc82
Arnav <jainarnav314@gmail.com>
2023-07-26 13:27:36 +0530

    added env file

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
+
+dotenv.config()
 
 const app = express();
 
@@ -14,7 +16,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb://127.0.0.1:27017/test';
+const CONNECTION_URL = process.env.MONGO_URI;
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
