a5cd4777380ac43a3bd9b4463262d72b90022a81
Arnav <jainarnav314@gmail.com>
2023-07-26 14:28:35 +0530

    build ready

---
+++ client/build/precache-manifest.eafbba07ab32495478dbbff567c4d9aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9897d55d2181aa81cf54aba7ef21ecce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bc1cfbfd6b5e3dd7304",
+    "url": "/static/css/main.4e89baf1.chunk.css"
+  },
+  {
+    "revision": "75a8e08f9842d5bacc31",
+    "url": "/static/js/2.399ef571.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.399ef571.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bc1cfbfd6b5e3dd7304",
+    "url": "/static/js/main.54d9a3b6.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
